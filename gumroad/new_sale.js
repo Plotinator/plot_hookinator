32b317ec1db333ce53c101b4f747383a3d37a9b8
Cameron Sutter <cameronsutter0@gmail.com>
2019-07-03 15:20:52 -0600

    TEST: see lists

--- gumroad/new_sale.js
+++ gumroad/new_sale.js
@@ -1,3 +1,8 @@
-module.exports = (req, res) => {
-  res.end(`The date is ${Date.now()}`)
+var Mailchimp = require('mailchimp-api-v3')
+var mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN)
+
+module.exports = async (req, res) => {
+  // let fakeEmail = `${req.body.product_id}@gmail.com`
+  let lists = await mailchimp.get('/lists')
+  res.status(200).send(lists)
 }

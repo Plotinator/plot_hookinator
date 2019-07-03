d244a57e1ad74a85ef5eb3f18eb6e8c58a758ca8
Cameron Sutter <cameronsutter0@gmail.com>
2019-07-03 15:56:48 -0600

    subscribe fake email

--- gumroad/new_sale.js
+++ gumroad/new_sale.js
@@ -1,8 +1,14 @@
 var Mailchimp = require('mailchimp-api-v3')
 var mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN)
 
+const PLOTTR_LIST_URL = 'https://us3.api.mailchimp.com/3.0/lists/4d4d9bfc24/members'
+const TRIAL_LIST_URL = 'https://us3.api.mailchimp.com/3.0/lists/e32e48b713/members'
+
 module.exports = async (req, res) => {
-  // let fakeEmail = `${req.body.product_id}@gmail.com`
-  let lists = await mailchimp.get('/lists')
-  res.status(200).send(lists)
+  let fakeEmail = `${req.body.product_id}@gmail.com`
+  let list = await mailchimp.post(PLOTTR_LIST_URL, {
+    email_address: fakeEmail,
+    status: 'subscribed',
+  })
+  res.status(200).send(list)
 }

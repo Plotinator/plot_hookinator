de8aff4502e09fdd76f0154b4ad8af85c69716aa
Cameron Sutter <cameronsutter0@gmail.com>
2019-07-03 16:12:57 -0600

    try subscribing again

--- gumroad/new_sale.js
+++ gumroad/new_sale.js
@@ -1,14 +1,29 @@
-var Mailchimp = require('mailchimp-api-v3')
-var mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN)
+const Mailchimp = require('mailchimp-api-v3')
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN)
+const md5 = require('js-md5')
 
+const PLOTTR_PRODUCT_ID = 'RxdkAsM4CXkeT-g9yy6NiQ=='
 const PLOTTR_LIST_URL = 'https://us3.api.mailchimp.com/3.0/lists/4d4d9bfc24/members'
 const TRIAL_LIST_URL = 'https://us3.api.mailchimp.com/3.0/lists/e32e48b713/members'
 
 module.exports = async (req, res) => {
-  let fakeEmail = `${req.body.product_id}@gmail.com`
-  let list = await mailchimp.post(PLOTTR_LIST_URL, {
-    email_address: fakeEmail,
-    status: 'subscribed',
-  })
-  res.status(200).send(list)
+  if (req.body.product_id == PLOTTR_PRODUCT_ID) {
+    let fakeEmail = `cameronsutter.author@gmail.com`
+    await mailchimp.post(PLOTTR_LIST_URL, {
+      email_address: fakeEmail,
+      status: 'subscribed',
+    })
+
+    // let hash = md5(req.body.email)
+    // try {
+    //   const memberUrl = `${TRIAL_LIST_URL}/${hash}`
+    //   let member = await mailchimp.get(memberUrl)
+    //   if (member.status == 'subscribed') {
+    //     await mailchimp.delete(memberUrl)
+    //   }
+    // } catch (err) {
+    //   // probably not subscribed ?
+    // }
+  }
+  res.status(200)
 }

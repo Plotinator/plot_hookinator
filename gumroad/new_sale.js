d13433e78205c2fc784c7e8f8f6702580d725c70
Cameron Sutter <cameronsutter0@gmail.com>
2019-07-03 15:31:35 -0600

    TEST add back lists

--- gumroad/new_sale.js
+++ gumroad/new_sale.js
@@ -3,6 +3,6 @@ var mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN)
 
 module.exports = async (req, res) => {
   // let fakeEmail = `${req.body.product_id}@gmail.com`
-  // let lists = await mailchimp.get('/lists')
-  res.status(200).send('success')
+  let lists = await mailchimp.get('/lists')
+  res.status(200).send(lists)
 }

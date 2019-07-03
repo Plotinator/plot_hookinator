4d1846c90a57502684fac13d0c5a356e90bcecda
Cameron Sutter <cameronsutter0@gmail.com>
2019-07-03 16:21:40 -0600

    try the right path

--- gumroad/new_sale.js
+++ gumroad/new_sale.js
@@ -3,8 +3,8 @@ const mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN)
 const md5 = require('js-md5')
 
 const PLOTTR_PRODUCT_ID = 'RxdkAsM4CXkeT-g9yy6NiQ=='
-const PLOTTR_LIST_URL = 'https://us3.api.mailchimp.com/3.0/lists/4d4d9bfc24/members'
-const TRIAL_LIST_URL = 'https://us3.api.mailchimp.com/3.0/lists/e32e48b713/members'
+const PLOTTR_LIST_URL = '/lists/4d4d9bfc24/members'
+const TRIAL_LIST_URL = '/lists/e32e48b713/members'
 
 module.exports = async (req, res) => {
   console.log('Product ID', req.body.product_id)

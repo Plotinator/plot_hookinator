030c389aa2d29fb65f7ab8ca5243cc92d9c19803
Cameron Sutter <cameronsutter0@gmail.com>
2019-07-17 14:04:05 -0600

    add moar logging

--- gumroad/new_sale.js
+++ gumroad/new_sale.js
@@ -8,7 +8,9 @@ const TRIAL_LIST_URL = '/lists/e32e48b713/members'
 
 module.exports = async (req, res) => {
   if (req.body.product_id == PLOTTR_PRODUCT_ID) {
+    // add to Plottr list
     try {
+      console.log(`adding ${req.body.email} to Plottr list`)
       await mailchimp.post(PLOTTR_LIST_URL, {
         email_address: req.body.email,
         status: 'subscribed',
@@ -17,16 +19,20 @@ module.exports = async (req, res) => {
       console.error(err)
     }
 
+    // Remove from FreeTrial list
     let hash = md5(req.body.email)
     try {
+      console.log(`attempting to find ${req.body.email} in free trial list`)
       const memberUrl = `${TRIAL_LIST_URL}/${hash}`
       let member = await mailchimp.get(memberUrl)
       if (member.status == 'subscribed') {
+        console.log(`removing ${req.body.email} from free trial list`)
         await mailchimp.delete(memberUrl)
       }
     } catch (err) {
       // probably not subscribed ?
-      console.log(err)
+      console.error('PROBABLY not subscribed')
+      console.error(err)
     }
   }
   res.status(200).send()
